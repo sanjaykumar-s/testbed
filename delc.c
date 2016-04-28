cc1d204a49d692d748a34812834bbb365ee9cd0b
codecademy <ccuser@codecademy.com>
2016-04-28 06:34:36 -0400

    Added delc.c and its binary

---
+++ delc.c
@@ -0,0 +1,8 @@
+# include<stdio.h>
+
+int main()
+{
+ printf(" This is a good start for C programing\n");
+ printf(" We are hosting the code in the github server for our reference\n");
+ return 0;
+}
